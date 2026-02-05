46ebb654340dfdff9159f67c5b73595ddaa3b54e
Maria-Liza-Lopez <marializalopez02@gmail.com>
2026-02-05 22:00:27 +0530

    The reservation process is being developed

--- src/com/oceanview/model/Reservation.java
+++ src/com/oceanview/model/Reservation.java
@@ -9,80 +9,27 @@ public class Reservation {
     private String address;
     private String contactNumber;
     private String roomType;
-    private Date checkInDate;
-    private Date checkOutDate;
-    private int numberOfNights;
-    private double totalAmount;
+    private Date checkIn;
+    private Date checkOut;
 
-    public int getReservationId() {
-        return reservationId;
-    }
+    public int getReservationId() { return reservationId; }
+    public void setReservationId(int reservationId) { this.reservationId = reservationId; }
 
-    public void setReservationId(int reservationId) {
-        this.reservationId = reservationId;
-    }
+    public String getGuestName() { return guestName; }
+    public void setGuestName(String guestName) { this.guestName = guestName; }
 
-    public String getGuestName() {
-        return guestName;
-    }
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
 
-    public void setGuestName(String guestName) {
-        this.guestName = guestName;
-    }
+    public String getContactNumber() { return contactNumber; }
+    public void setContactNumber(String contactNumber) { this.contactNumber = contactNumber; }
 
-    public String getAddress() {
-        return address;
-    }
+    public String getRoomType() { return roomType; }
+    public void setRoomType(String roomType) { this.roomType = roomType; }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+    public Date getCheckIn() { return checkIn; }
+    public void setCheckIn(Date checkIn) { this.checkIn = checkIn; }
 
-    public String getContactNumber() {
-        return contactNumber;
-    }
-
-    public void setContactNumber(String contactNumber) {
-        this.contactNumber = contactNumber;
-    }
-
-    public String getRoomType() {
-        return roomType;
-    }
-
-    public void setRoomType(String roomType) {
-        this.roomType = roomType;
-    }
-
-    public Date getCheckInDate() {
-        return checkInDate;
-    }
-
-    public void setCheckInDate(Date checkInDate) {
-        this.checkInDate = checkInDate;
-    }
-
-    public Date getCheckOutDate() {
-        return checkOutDate;
-    }
-
-    public void setCheckOutDate(Date checkOutDate) {
-        this.checkOutDate = checkOutDate;
-    }
-
-    public int getNumberOfNights() {
-        return numberOfNights;
-    }
-
-    public void setNumberOfNights(int numberOfNights) {
-        this.numberOfNights = numberOfNights;
-    }
-
-    public double getTotalAmount() {
-        return totalAmount;
-    }
-
-    public void setTotalAmount(double totalAmount) {
-        this.totalAmount = totalAmount;
-    }
+    public Date getCheckOut() { return checkOut; }
+    public void setCheckOut(Date checkOut) { this.checkOut = checkOut; }
 }
--- webapp/dashboard.jsp
+++ webapp/dashboard.jsp
@@ -15,6 +15,15 @@
 
 <h2>Welcome, <%= user.getUsername() %></h2>
 <p>Role: <%= user.getRole() %></p>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand">Ocean View Resort</a>
+  <ul class="navbar-nav">
+    <li class="nav-item"><a class="nav-link" href="dashboard.jsp">Home</a></li>
+    <li class="nav-item"><a class="nav-link" href="reservation.jsp">Reservation</a></li>
+    <li class="nav-item"><a class="nav-link" href="../viewReservations">Reservation Details</a></li>
+    <li class="nav-item"><a class="nav-link" href="../logout">Logout</a></li>
+  </ul>
+</nav>
 
 <a href="logout">Logout</a>
 
